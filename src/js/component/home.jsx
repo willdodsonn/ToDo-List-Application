0acc7deaed75cc658172f18b1892d2a3fdced1d8
willdodsonn <sodlax38@gmail.com>
2022-02-21 15:19:09 +0000

    start todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="container">
+			<h1 className="todo-header">todos</h1>
+			<input id="addToDo" type="text" placeholder="Add to do here" />
+			<ul className="list-group">
+				<li className="list-group-item">
+					<span>
+						<FontAwesomeIcon icon={faX} />
+					</span>{" "}
+					Walk the dog
+				</li>
+				<li className="list-group-item">
+					<span>
+						<FontAwesomeIcon icon={faX} />
+					</span>{" "}
+					Buy superpowers
+				</li>
+				<li className="list-group-item">
+					<span>
+						<FontAwesomeIcon icon={faX} />
+					</span>{" "}
+					Invest life savings into NFT's
+				</li>
+			</ul>
 		</div>
 	);
 };
 
 export default Home;
+
+//hard coded bootstrap/ html output
