c39a32b7fb7982cf16e5130545b76beca205095e
willdodsonn <sodlax38@gmail.com>
2022-02-22 03:53:43 +0000

    eh, getting there

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,18 +9,20 @@ const Home = () => {
 		<div className="todoListBox">
 			<div id="container">
 				<h1 className="todo-header">todos</h1>
+				<button type="submit">ADD</button>
 				<input id="addToDo" type="text" placeholder="Add to do here" />
 				<ul className="list-group">
 					<li className="list-group-item">
-						<span>Walk the dog</span> <FontAwesomeIcon icon={faX} />
+						<span> Walk the dog</span>{" "}
+						<FontAwesomeIcon icon={faX} className="icon" />
 					</li>
 					<li className="list-group-item">
 						<span> Buy superpowers</span>{" "}
-						<FontAwesomeIcon icon={faX} />
+						<FontAwesomeIcon icon={faX} className="icon" />
 					</li>
 					<li className="list-group-item">
 						<span> Invest life savings into NFT's</span>{" "}
-						<FontAwesomeIcon icon={faX} />
+						<FontAwesomeIcon icon={faX} className="icon" />
 					</li>
 				</ul>
 			</div>
